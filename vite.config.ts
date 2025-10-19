b55c1380c1ecdecc3b5c7bbd56d1ea453b73f91f
Vunguyentai123 <atgvuviplion@gmail.com>
2025-10-19 16:34:56 +0700

    Add GitHub Pages config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/20-10-ver2/',
   plugins: [react()],
   server: {
     host: true,
